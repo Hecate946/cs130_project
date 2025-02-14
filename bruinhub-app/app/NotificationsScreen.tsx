09f81dbe1bea05ed6da84367e69d9326428a3451
ingriddleee <lee.ingrid2003@gmail.com>
2025-02-13 23:38:25 -0800

    add scrollable elements and other similar components following onboarding screen, with lots of comments -- dirty
    Change-Id: I294d14b35c05dbc0951d6953c6d615e4f62d5175

--- bruinhub-app/app/NotificationsScreen.tsx
+++ bruinhub-app/app/NotificationsScreen.tsx
@@ -6,19 +6,92 @@ import * as Haptics from "expo-haptics";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Categories } from "@/constants/Categories";
 import NotificationItem from "@/components/notifications/NotificationItem";
+import Divider from "@/components/Divider";
 
-const array_categories = ["Fitness", "Dining", "Study Rooms"];
-const array_restaurants = [{name: "B-Plate", "id": '0'}, {name: "De Neve", "id": '1'}, {name: "Epicuria", "id": '2'},{name: "The Study", "id": '3'},{name: "Rendezvous", "id": '4'},];
+//const array_categories = ["Fitness", "Dining", "Study Rooms"];
+//const array_restaurants = [{name: "B-Plate", "id": '0'}, {name: "De Neve", "id": '1'}, {name: "Epicuria", "id": '2'},{name: "The Study", "id": '3'},{name: "Rendezvous", "id": '4'},];
 
-// const Restaurants = Categories.diningHalls;
-// const Takeout = Categories.takeout;
-// const Gyms = Categories.gym;
-// const Libraries = Categories.libraries;
-// const ReslifeStudy = Categories.reslifeStudy;
-// const ReslifeSpaces = Categories.reslifeSpaces;
+const Restaurants = Categories.diningHalls;
+const Takeout = Categories.takeout;
+const Gyms = Categories.gym;
+const Libraries = Categories.libraries;
+const ReslifeStudy = Categories.reslifeStudy;
+const ReslifeSpaces = Categories.reslifeSpaces;
 
 export default function NotificationsScreen() {
   return (
+    <View style={styles.container}>
+      <ScrollView
+        style={styles.scrollContainer}
+        contentContainerStyle={styles.contentContainer}
+        //keyboardShouldPersistTaps="handled"
+      >
+        <View style={styles.welcomeContainer}>
+          <Text style={styles.title}>Notifications</Text>
+          <Text style={styles.text}>Select the facilities you'd like to receive 
+            notifications for, including hours updates and holiday reminders.</Text>
+        </View>
+
+        <Divider name="Restaurants" />
+        <View /*style={styles.chipContainer}*/>
+          {Restaurants.map((item) => (
+            <NotificationItem name={item.name} key={item.key}/>
+          ))}
+        </View> 
+
+        <Divider name="Takeout" />
+        <View /*style={styles.chipContainer}*/>
+          {Takeout.map((item) => (
+            <NotificationItem name={item.name} key={item.key}/>
+          ))}
+        </View>
+
+        <Divider name="Gyms" />
+        <View /*style={styles.chipContainer}*/>
+          {Gyms.map((item) => (
+            <NotificationItem name={item.name} key={item.key}/>
+          ))}
+        </View>
+
+        <Divider name="Libraries" />
+        <View /*style={styles.chipContainer}*/>
+          {Libraries.map((item) => (
+            <NotificationItem name={item.name} key={item.key}/>
+          ))}
+        </View>
+
+        <Divider name="ResLife Study Rooms" />
+        <View /*style={styles.chipContainer}*/>
+          {ReslifeStudy.map((item) => (
+            <NotificationItem name={item.name} key={item.key}/>
+          ))}
+        </View>
+
+        <Divider name="ResLife Spaces" />
+        <View /*style={styles.chipContainer}*/>
+          {ReslifeSpaces.map((item) => (
+            <NotificationItem name={item.name} key={item.key}/>
+          ))}
+        </View>
+
+      </ScrollView>
+      <View style={{ position: "absolute", bottom: 50 }}>
+        <Link href="/" asChild>
+          <Button
+            style={styles.button}
+            //onPress={handleFinish}
+            uppercase={true}
+            mode="elevated"
+            //contentStyle={styles.buttonContent}
+            labelStyle={styles.buttonLabel}
+          >
+            Done
+          </Button>
+        </Link>
+      </View>
+    </View>
+  );
+  /*return (
     <View style={styles.container}>
       <View style = {styles.leftAlignedText}>
       <Text>Notifications</Text>
@@ -39,25 +112,47 @@ export default function NotificationsScreen() {
           <Text>Done</Text>
         </Button>
       </Link>
-    </View>
-  );
+    </View> 
+  ); */
 }
 
 const styles = StyleSheet.create({
-  leftAlignedText: {
-    padding: 12,
-    justifyContent: "flex-start", //puts in center of screen up/down wise.
-    alignItems: "flex-start",//alignItems: "flex-start", //aligns actual text and buttons to center left/right wise.
-    //backgroundColor: Colors.light.background,
-    width: "100%"
-  },
   container: {
-    padding: 12,
+    //padding: 12,
     flex: 1,
-    justifyContent: "center", //puts in center of screen up/down wise.
+    justifyContent: "flex-start", //puts in center of screen up/down wise.
     alignItems: "center",//alignItems: "flex-start", //aligns actual text and buttons to center left/right wise.
     backgroundColor: Colors.light.background,
-    width: "100%"
+    width: "100%",
+    paddingTop: 80, //TODO CHANGE LATER
+  },
+
+  scrollContainer: {
+    flex: 1,
+  },
+
+  contentContainer: {
+    flexGrow: 1,
+    paddingBottom: 155,
+    marginStart: 20,
+    marginEnd: 20,
+  },
+
+  welcomeContainer: {
+    backgroundColor: Colors.light.background,
+    width: "100%",
+    gap: 5,
+  },
+  
+  title: {
+    fontSize: 24,
+    fontWeight: 400,
+    textTransform: "uppercase",
+  },
+
+  text: {
+    fontSize: 16,
+    fontWeight: 300,
   },
 
   button: {
