c24f42c5147e123b28238476a7f3d65e00aba4a1
ingriddleee <lee.ingrid2003@gmail.com>
2025-02-13 23:06:17 -0800

    init impl of notif screen for dining halls only Change-Id: Ic871e31b4c027a6872d15d08d244b3dfa8536810
    add scrollable elements and other similar components following onboarding screen, with lots of comments -- dirty
    
    Change-Id: I294d14b35c05dbc0951d6953c6d615e4f62d5175
    
    add haptic feedback for done button
    
    Change-Id: I6d96c7a9b86d58ffd33e9f230409f3c8d6d95a0d
    
    add local storage saving for notification screen, also had to add ids to notificationItem components
    
    Change-Id: I6fdd1aa82fafae7881a94b8d5997b0f7dce988d8
    
    added functionality to keep notifications on after leaving the screen, also remove lag when accessing async storage by moving the set state before the access
    
    Change-Id: Idb6d16a16be9db50581d3935b13157f4b4019d5d
    
    clean & remove comments, change color of toggle so it is more noticable and can be quickly looked at by user
    
    Change-Id: I0ed031930ebe28f1926030b2193721ee6aa4adab

--- bruinhub-app/app/NotificationsScreen.tsx
+++ bruinhub-app/app/NotificationsScreen.tsx
@@ -1,22 +1,160 @@
-import { Text, View } from "react-native";
+import { Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
 import { Link } from "expo-router";
+import { Colors } from "@/constants/Colors";
 import { Button } from "react-native-paper";
+import * as Haptics from "expo-haptics";import { Categories } from "@/constants/Categories";
+import NotificationItem from "@/components/notifications/NotificationItem";
+import Divider from "@/components/Divider";
 
-export default function NotificationsScreen() {
+const Restaurants = Categories.diningHalls;
+const Takeout = Categories.takeout;
+const Gyms = Categories.gym;
+const Libraries = Categories.libraries;
+const ReslifeStudy = Categories.reslifeStudy;
+const ReslifeSpaces = Categories.reslifeSpaces;
+
+interface NotificationsScreenProps {
+  onFinish: () => void;
+};
+
+export default function NotificationsScreen({ onFinish }: NotificationsScreenProps) {
+  
+  const handleFinish = async () => {
+    await Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Heavy);
+    onFinish();
+  };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>This is the notifications screen</Text>
-      <Link href="/HomeScreen" asChild>
-        <Button mode="contained">
-            Home
-        </Button>
-      </Link>
+    <View style={styles.container}>
+      <ScrollView
+        style={styles.scrollContainer}
+        contentContainerStyle={styles.contentContainer}
+      >
+        <View style={styles.welcomeContainer}>
+          <Text style={styles.title}>Notifications</Text>
+          <Text style={styles.text}>Select the facilities you'd like to receive 
+            notifications for, including hours updates and holiday reminders.</Text>
+        </View>
+
+        <Divider name="Restaurants" />
+        <View>
+          {Restaurants.map((item) => (
+            <NotificationItem name={item.name} key={item.key} id={item.key}/>
+          ))}
+        </View> 
+
+        <Divider name="Takeout" />
+        <View>
+          {Takeout.map((item) => (
+            <NotificationItem name={item.name} key={item.key} id={item.key}/>
+          ))}
+        </View>
+
+        <Divider name="Gyms" />
+        <View>
+          {Gyms.map((item) => (
+            <NotificationItem name={item.name} key={item.key} id={item.key}/>
+          ))}
+        </View>
+
+        <Divider name="Libraries" />
+        <View>
+          {Libraries.map((item) => (
+            <NotificationItem name={item.name} key={item.key} id={item.key}/>
+          ))}
+        </View>
+
+        <Divider name="ResLife Study Rooms" />
+        <View>
+          {ReslifeStudy.map((item) => (
+            <NotificationItem name={item.name} key={item.key} id={item.key}/>
+          ))}
+        </View>
+
+        <Divider name="ResLife Spaces" />
+        <View>
+          {ReslifeSpaces.map((item) => (
+            <NotificationItem name={item.name} key={item.key} id={item.key}/>
+          ))}
+        </View>
+
+      </ScrollView>
+      <View style={{ position: "absolute", bottom: 50 }}>
+        <Link href="/" asChild>
+          <Button
+            style={styles.button}
+            onPress={handleFinish}
+            uppercase={true}
+            mode="elevated"
+            labelStyle={styles.buttonLabel}
+          >
+            Done
+          </Button>
+        </Link>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "flex-start", //puts in center of screen up/down wise.
+    alignItems: "center",//alignItems: "flex-start", //aligns actual text and buttons to center left/right wise.
+    backgroundColor: Colors.light.background,
+    width: "100%",
+    paddingTop: 80, //TODO CHANGE LATER
+  },
+
+  scrollContainer: {
+    flex: 1,
+  },
+
+  contentContainer: {
+    flexGrow: 1,
+    paddingBottom: 155,
+    marginStart: 20,
+    marginEnd: 20,
+  },
+
+  welcomeContainer: {
+    backgroundColor: Colors.light.background,
+    width: "100%",
+    gap: 5,
+  },
+  
+  title: {
+    fontSize: 24,
+    fontWeight: 400,
+    textTransform: "uppercase",
+  },
+
+  text: {
+    fontSize: 16,
+    fontWeight: 300,
+  },
+
+  button: {
+    marginTop: 20,
+    padding: 10,
+    height: 64,
+    width: 200,
+    backgroundColor: Colors.light.icon,
+    borderRadius: 60,
+  },
+
+  buttonLabel: {
+    fontSize: 18,
+    color: Colors.light.text,
+    alignItems: "center",
+  },
+
+  bannerText: {
+    height: 64,
+    width: 100,
+    alignItems: "flex-start",
+    color: Colors.dark.icon,
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+});
