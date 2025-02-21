b02fd3a0f74d5272b95f593e3dc109fe6bbc323f
Paul Zhang <paul.zy.zhang@gmail.com>
2025-02-20 16:29:01 -0800

    Add scraper for dine-in restaurants
    Change-Id: Id00000002649d33f56b411bd04e11ca9290e56a6

---
+++ bruinhub-backend/scrapers/restaurants.py
@@ -0,0 +1,75 @@
+import requests
+from bs4 import BeautifulSoup
+from test.test_dis import get_tb
+
+RESTAURANTS = ["BruinPlate", "DeNeve", "Epicuria"]
+
+# From https://testing.occuspace.io/waitz/customer/21/locations
+OCCUSPACE_IDS: dict[str, int] = {
+    "Powell CLICC": 104,
+    "The Drey": 81,
+    "Feast": 100,
+    "Epicuria": 88,
+    "Epicuria at Ackerman": 108,
+    "Bruin Cafe": 79,
+    "Rendezvous": 82,
+    "BruinPlate": 77,
+    "DeNeve": 78,
+    "The Study": 83,
+}
+
+# From https://testing.occuspace.io/waitz/customer/21/locations
+OCCUSPACE_IDS: dict[str, int] = {
+    "Epicuria": 88,
+    "BruinPlate": 77,
+    "DeNeve": 78,
+}
+
+
+def _hours_today(restaurant: str) -> list[str]:
+    """
+    Returns the hours of operation that apply today
+    """
+    data = requests.get(f"https://testing.occuspace.io/waitz/location/{OCCUSPACE_IDS[restaurant]}/hours").json()
+    return next(rules["hours"] for rules in data["data"][0]["rules"] if rules["active"] == True)
+
+def _occupancy(restaurant: str) -> tuple[int, int]:
+    """
+    Returns number of people and the capacity of the restaurant.
+    """
+    data = requests.get(f"https://testing.occuspace.io/waitz/location/{OCCUSPACE_IDS[restaurant]}").json()
+    return data["data"]["people"], data["data"]["capacity"]
+
+def _menu(restaurant: str) -> dict[str, list[str]]:
+    """
+    Returns the menu for the restaurant.
+    """
+    soup = BeautifulSoup(
+        requests.get(f"https://menu.dining.ucla.edu/Menus/{restaurant}").text, "lxml"
+    )
+
+    return {
+        str(section.contents[0]).strip(): list(
+            map(lambda elem: elem.text, section.select("a"))
+        )
+        for section in soup.select("li.sect-item")
+    }
+
+def get_restaurants():
+    """
+    Returns occupany, menu, and hours data for all restaurants
+    """
+    occupants, capacity = _occupancy("BruinPlate")
+    return {
+        restaurant: {
+            "hours_today": _hours_today(restaurant),
+            "occupants": occupants,
+            "capacity": capacity,
+            "menu": _menu(restaurant),
+        }
+        for restaurant in RESTAURANTS
+    }
+
+
+if __name__ == "__main__":
+    print(get_restaurants())
